d7158c611c1a81ee3f59b757d1bbdd5febdb01c7
Riley <rileymshea@gmail.com>
2022-02-06 01:51:28 -0500

    add create_style_commit script

---
+++ scripts/create_style_commit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+
+# Make a commit that only contains the style changes.
+# This will run formatting across all files and handle all setup for making
+# git-blame retain attribution to the creator of functional commits that would
+# otherwise be hidden during `git blame`.
+
+PROJECT_DIR=$(git rev-parse --show-toplevel)
+
+npm run prettier -- --write . &&
+    git add . &&
+    git commit -m "Repo-wide style changes." &&
+    git config --global blame.ignorerevsfile .git-blame-ignore-revs &&
+    touch "$PROJECT_DIR.git-blame-ignore-revs" &&
+    git log -1 --pretty=%H >>"$PROJECT_DIR/.git-blame-ignore-revs"
